ca257ce80bcf35941554e3faddd0b287db31c6cf
nar <yoyo7227@naver.com>
2021-01-06 19:32:23 +0900

    add habit-tracker

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import '@fortawesome/fontawesome-free/js/all';
+import SimpleHabit from "./components/simpleHabit";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <SimpleHabit/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
